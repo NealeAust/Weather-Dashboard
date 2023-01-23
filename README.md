46efad875490d6706d95734e588b88638a67a2a0
NealeAust <nealechapman@optushome.com.au>
2023-01-24 09:06:34 +1100

    coding updated & icon added

--- README.md
+++ README.md
@@ -1,1 +1,86 @@
 # Weather-Dashboard
+
+## Project Title
+
+
+
+
+## APIs Used
+
+### Third-Party APIs
+
+- CSS Framework: BootStrap
+
+- JavaScript Library: jQuery
+
+### Server-Side APIs
+
+Get longitude(lon) & latitude(lat)
+- "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + APIKey;
+
+Get weather details
+- "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&exclude=hourly,daily&appid=" + APIKey + "&units=metric";
+
+Get weather icon, "ironcode" variable containing pathway to icon field 
+- "http://openweathermap.org/img/w/" + iconcode + ".png";
+
+
+## Superhero Smackdown
+
+
+
+
+
+
+
+
+## Project Description
+
+
+
+**Note**: A description of how to use the completed web application appears in the 'Usage' section below.
+
+The structure of the coding and what each section does is as follows:
+
+1. Prepare HTML coding including links to jQuery and Bootstrap. 
+
+2. 
+
+3. 
+
+4.
+
+5. 
+
+6.
+
+7. 
+
+## Installation
+
+N/A
+
+## Usage
+
+
+
+
+
+
+## Deployed Application and Screenshots
+
+### Link to the deployed application
+
+Link:  
+
+### Screenshot
+
+
+
+
+
+## Licence
+
+
+
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,28 @@
+// Assign API key number to a const variable, to save
+// re-typing & possible mistyping.
 const APIKey = "74ad0572a84c4528b742eb829895e376";
 
+// Obtain City Name from Search Input
 var submitButtonEl = document.querySelector("#submit");
 submitButtonEl.addEventListener("click", function (event) {
   event.preventDefault();
-
-  //   Obtain City Name from Search Input
-
   var city = document.querySelector("#cityName").value.trim();
 
+  // Get saved search history, and check if empty  
   var cityList = JSON.parse(localStorage.getItem("listcities"))
-
   if (cityList === null) {
     cityList = [];
+
   }
 
+  // Called latititute(lat) and longitude(lon) for city searched using API
+  // Display city name + lat & lon in console.log
   var geoUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + APIKey;
 
   fetch(geoUrl)
     .then(function (response) {
-      // console.log(response)
       return response.json();
+
     })
 
     .then(function (data) {
@@ -27,32 +30,35 @@ submitButtonEl.addEventListener("click", function (event) {
       console.log(data[0].name);
       const lat = data[0].lat;
       const lon = data[0].lon;
-
-
       console.log(lat);
       console.log(lon);
 
+      //Called current days weather details 'openweathermap' by building URL with parameters
       const weatherUrl = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&exclude=hourly,daily&appid=" + APIKey + "&units=metric";
       console.log(weatherUrl);
 
       fetch(weatherUrl)
         .then(function (response) {
           return response.json();
+
         })
+
         .then(function (data) {
 
+          // Display city searched and current date
           var cityWeather = $("#cityWeather")
           cityWeather.text(data.city.name);
           var currentDate = document.querySelector("h2");
           currentDate.textContent = dayjs().format("(MM/DD/YYYY)");
-          const iconcode = data.list[0].weather[0].icon             
+
+          // Called and displayed weather icon
+          const iconcode = data.list[0].weather[0].icon
           var iconUrl = "http://openweathermap.org/img/w/" + iconcode + ".png";
           $("#weatherIcon").attr("src", iconUrl);
-                
+
           console.log(data);
-          console.log(data.list[0].weather[0].icon)
-          
-                    
+
+          // Display current day weather information on dashboard
           var temp = $("#temperature");
           temp.text("Temp:  " + data.list[0].main.temp + "  \u00B0C");
 
@@ -62,122 +68,49 @@ submitButtonEl.addEventListener("click", function (event) {
           var humidity = $("#humidity");
           humidity.text("Humidity:  " + data.list[0].main.humidity + "  %");
 
-          
-          // console.log(data.list[0].wind.speed);
-
-          // 5 day forcast
-
+          // Five day weather forecast
+          // Used for loop to get details for each day
+          // "Fcst" is an abbreviation for "Forecast"
           for (var i = 1; i <= 5; i++) {
-            // add class to future cards to create card containers
-            var cardFc = $(".cardFc");
-            cardFc.addClass("cardFc");
 
-            var dateFc = $("#dateFc-" + i);
+            // Get and display each days date
+            var dateFcst = $("#dateFcst-" + i);
             date = dayjs().add(i, "d").format("(MM/DD/YYYY)");
-            dateFc.text(date);
+            dateFcst.text(date);
 
-            // add temp to 5 day forecast
-            var tempFc = $("#tempFc-" + i);
-            tempFc.text("Temp:  " + data.list[i].main.temp + "");
+            //Get and display weather icon for each day  
+            var iconFcst = $("#iconFcst-" + i);
+            const iconcodeFcst = data.list[i].weather[0].icon;
+            var iconUrlFcst = "http://openweathermap.org/img/w/" + iconcodeFcst + ".png";
+            iconFcst.attr("src", iconUrlFcst);
 
-            // add wind speed to 5 day forecast
-            var windFc = $("#windFc-" + i);
-            windFc.text("Wind:  " + data.list[i].wind.speed + "  KMH");
+            // Get and display forecast temp for each day 
+            var tempFcst = $("#tempFcst-" + i);
+            tempFcst.text("Temp:  " + data.list[i].main.temp + "  \u00B0C");
 
-            // add humidity to 5 day forecast
-            var humidityFc = $("#humidityFc-" + i);
-            humidityFc.text("Humidity:  " + data.list[i].main.humidity + "  %");
+            // Get and display forecast wind-speed for each day
+            var windFcst = $("#windFcst-" + i);
+            windFcst.text("Wind:  " + data.list[i].wind.speed + "  KMH");
 
+            // Get and display forecast humidity for each day  
+            var humidityFcst = $("#humidityFcst-" + i);
+            humidityFcst.text("Humidity:  " + data.list[i].main.humidity + "  %");
 
           }
 
-
+          //Add name of city searched into local storage
           cityList.push(city)
           localStorage.setItem("listcities", JSON.stringify(cityList))
+
+          // Create and display a search history
           for (var i = 0; i < cityList.length; i++) {
             var entry = document.createElement('h5')
             entry.textContent = cityList[i]
-          //  console.log(cityList[i]);
-           
             entry.classList.add("searchHistory")
-
-
-             document.getElementById("searchHistory").appendChild(entry)
-          }
-
-
-
+            document.getElementById("searchHistory").appendChild(entry)
+            
+            }
+        
         });
     });
-
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
